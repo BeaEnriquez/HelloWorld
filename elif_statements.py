5e35fcb8f685780aa4fc4866eb4754d2c9cd35cb
Bea Enriquez <31570224+BeaEnriquez@users.noreply.github.com>
2018-01-02 16:07:30 +0800

    Add files via upload

---
+++ elif_statements.py
@@ -0,0 +1,10 @@
+def flying_snake():
+
+    if 5 > 4 and 2 > 3:
+        return True
+    elif 7 == 7:
+        return False
+    else:
+        return False
+
+    # combinations of comparators, boolean operators and conditional statements
