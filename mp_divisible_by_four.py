97e01cc836ec1f0d781d1b852ab56204425c295d
Bea Enriquez <31570224+BeaEnriquez@users.noreply.github.com>
2018-01-05 21:44:01 +0800

    Add files via upload

---
+++ mp_divisible_by_four.py
@@ -0,0 +1,6 @@
+
+num = int(input("Enter a number: "))
+if num % 4 == 0:
+    print ("The inputted integer is divisible by 4")
+else:
+    print ("The inputted integer is not divisible by 4")
---
+++ mp_divisible_by_three.py
@@ -0,0 +1,7 @@
+
+
+num = int(input("Enter a number: "))
+if num % 3 == 0:
+    print ("The inputted integer is divisible by 3")
+else:
+    print ("The inputted integer is not divisible by 3")
