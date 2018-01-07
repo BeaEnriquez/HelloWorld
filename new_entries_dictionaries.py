7b7216609c76528235825240f7fc4dc36f9a5540
Bea Enriquez <31570224+BeaEnriquez@users.noreply.github.com>
2018-01-07 15:17:16 +0800

    Add files via upload

---
+++ new_entries_dictionaries.py
@@ -0,0 +1,10 @@
+menu = {}
+menu['Chicken Alfredo'] = 14.50
+print (menu['Chicken Alfredo'])
+
+menu['Tinola'] = 15
+menu['Nilaga'] = 17.75
+menu['Bulalo'] = 20
+
+print ("There are " + str(len(menu)) + " items on the menu.")
+print (menu)
