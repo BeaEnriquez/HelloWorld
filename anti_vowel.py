e760716805024cac3b372c047990e0109a8dc3e2
Bea Enriquez <31570224+BeaEnriquez@users.noreply.github.com>
2018-01-23 22:27:21 +0800

    Add files via upload

---
+++ anti_vowel.py
@@ -0,0 +1,9 @@
+def anti_vowel(text): 
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	chars = []
+	for i in text: 
+		if i.lower() not in vowels:
+			chars.append(i)	
+	return "".join(chars)
+
+print(anti_vowel("Hello World!"))
